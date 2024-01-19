2dac1db6d298dc5036b0201df970d8f9e67dabbf
Olof <olofsva@chalmers.se>
2024-01-19 11:42:39 +0100

    Uppgift 2 Lab 1

--- Car.java
+++ Car.java
@@ -1,12 +1,15 @@
 import java.awt.*;
 
-public class Car {
+public class Car implements Movable {
     protected boolean turboOn;    // If the car has a turbo
     protected int nrDoors;        // Number of doors on the car
     protected double enginePower; // Engine power of the car
     protected double currentSpeed; // The current speed of the car
     protected Color color;        // Color of the car
     protected String modelName;  // Car model name
+    protected int xPos;  // x-coordinate of car
+    protected int yPos;  // y-coordinate of car
+    protected int[] direction;  // Direction of car, type array with two ints, e.g. {0, 1}
 
     /**
      * The constructor for the class car
@@ -16,13 +19,53 @@ public class Car {
      * @param color Color of the car
      * @param modelName Car model name
      */
-    protected Car(boolean turboOn, int nrDoors,double enginePower,Color color, String modelName) {
+    protected Car(boolean turboOn, int nrDoors,double enginePower,Color color, String modelName, int xPos, int yPos, int[] direction) {
         this.nrDoors = nrDoors;
         this.color = color;
         this.enginePower = enginePower;
         this.turboOn = turboOn;
         this.modelName = modelName;
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.direction = direction;
         //stopEngine();
+    }
+
+    public void move() {
+        this.xPos = this.xPos + currentSpeed * this.direction[0];
+        this.yPos = this.yPos + currentSpeed * this.direction[1];
+    }
+
+    public void turnLeft() {
+        if this.xPos == 0 && this.yPos == 1 {  // North to West
+            this.xPos = -1;
+            this.yPos = 0;
+        } else if this.xPos == -1 && this.yPos == 0 {  // West to South
+            this.xPos = 0;
+            this.yPos = -1;
+        } else if this.xPos == 0 && this.yPos == -1 {  // South to East
+            this.xPos = 1;
+            this.yPos = 0;
+        } else {  // East to North
+            this.xPos = 0;
+            this.yPos = 1;
+        }
+    }
+
+    public void turnRight() {
+        if this.xPos == 0 && this.yPos == 1 {  // North to East
+            this.xPos = 1;
+            this.yPos = 0;
+        } else if this.xPos == 1 && this.yPos == 0 {  // East to South
+            this.xPos = 0;
+            this.yPos = -1;
+        } else if this.xPos == 0 && this.yPos == -1 {  // South to West
+            this.xPos = -1;
+            this.yPos = 0;
+        } else {  // West to North
+            this.xPos = 0;
+            this.yPos = 1;
+        }
     }
 
 }
