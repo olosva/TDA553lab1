70b96a5e1b832404e4d028c1c6a6ba76560a62b2
jakobseevers <jakob.seevers@gmail.com>
2024-01-31 16:22:27 +0100

    Jakob

--- WorkShop.java
+++ WorkShop.java
@@ -3,21 +3,24 @@ import java.util.List;
 
 public class WorkShop<T extends Car> implements Loadable<T> {
     protected int maxCars;
+    int xPos;
+    int yPos;
 
     protected List<T> carsInShop = new ArrayList<>();
 
-    public void Workshop(int maxCars) {
+    public void Workshop(int maxCars, int xPos, int yPos) {
         this.maxCars = maxCars;
+        this.xPos = xPos;
+        this.yPos = yPos;
 
     }
 
     public void load(T car) {
-        if (carsInShop.size() <= maxCars -1){
-            carsInShop.add(car);
-
+        if (carsInShop.size() <= maxCars -1) {
+            if (car.xPos > this.xPos-2 && car.xPos < this.xPos+2 && car.yPos > this.yPos-2 && car.yPos < this.yPos+2) {
+                carsInShop.add(car);
+            }
         }
-
-
     }
     public void unload(){
         carsInShop.removeFirst();
