2065cb326c653b2a258bfca44b0211d392f0b2fb
Olof <olofsva@chalmers.se>
2024-02-01 08:57:00 +0100

    Olof

--- Test/WorkShopTest.java
+++ Test/WorkShopTest.java
@@ -1,4 +1,17 @@
+import org.junit.Test;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class WorkShopTest {
+
+    @Test
+    public void TestStaticErrorWhenLoadingWrongCarInShop() {
+        WorkShop<Volvo240> shop = new WorkShop<>(10, 0, 0);
+        Saab95 saab = new Saab95();
+        // shop.load(saab);
+    }
+
 
 
 }
