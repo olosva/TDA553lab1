a158668f72ee2104f5c2d5c263cb50c2c09f4f13
jakobseevers <jakob.seevers@gmail.com>
2024-02-08 11:39:44 +0100

    Jakob

--- Lab2-Old/CarController.java
+++ Lab2-Old/CarController.java
@@ -3,11 +3,12 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.lang.Math;
 
 /*
-* This class represents the Controller part in the MVC pattern.
-* It's responsibilities is to listen to the View and responds in a appropriate manner by
-* modifying the model state and the updating the view.
+ * This class represents the Controller part in the MVC pattern.
+ * It's responsibilities is to listen to the View and responds in a appropriate manner by
+ * modifying the model state and the updating the view.
  */
 
 public class CarController {
@@ -18,7 +19,6 @@ public class CarController {
     // The timer is started with a listener (see below) that executes the statements
     // each step between delays.
     private Timer timer = new Timer(delay, new TimerListener());
-
 
     // The frame that represents this instance View of the MVC pattern
     CarView frame;
@@ -32,21 +32,18 @@ public class CarController {
         // Instance of this class
         CarController cc = new CarController();
 
-
         Saab95 saab = new Saab95();
         saab.xPos = 100;
         Scania scania = new Scania();
         scania.xPos = 200;
-        cc.cars.add(new Volvo240());
+        Volvo240 volvo = new Volvo240();
+        volvo.xPos = 300;
+        volvo.yPos = 200;
+
+        cc.cars.add(volvo);
         cc.cars.add(saab);
         cc.cars.add(scania);
         cc.workshops.add(new WorkShop<Volvo240>(10, 300, 300));
-
-//        Car c = new Saab95();
-//        System.out.println(cc.cars.get(0) instanceof Volvo240);
-
-
-
 
         // Start a new view and send a reference of self
         cc.frame = new CarView("CarSim 1.0", cc);
@@ -56,33 +53,30 @@ public class CarController {
     }
 
     /* Each step the TimerListener moves all the cars in the list and tells the
-    * view to update its images. Change this method to your needs.
-    * */
+     * view to update its images. Change this method to your needs.
+     * */
     private class TimerListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
 
             for (Vehicle car : cars) {
                 double xPos = car.xPos + car.currentSpeed * car.direction[0];
                 double yPos = car.yPos + car.currentSpeed * car.direction[1];
-                if (car instanceof Car){
-                    if (checkLoadWorkshop((Car) car));
+                if (car instanceof Car) {
+                    if (checkLoadWorkshop((Car) car)) {
+                        workshops.getFirst().load((Volvo240) car);
+                        car.stopEngine();
+                    }
                 }
 
-
-                if (0 <= xPos && xPos <= frame.X && 0 <= yPos && yPos <= frame.Y - 300 ) {
+                if (0 <= xPos && xPos <= frame.X && 0 <= yPos && yPos <= frame.Y - 300) {
                     car.move();
-                    int x = (int) Math.round(car.xPos);
-                    int y = (int) Math.round(car.yPos);
-                    frame.drawPanel.moveit(car, x, y);
+
                 } else {
                     car.turnLeft();
                     car.turnLeft();
                     car.stopEngine();
                     car.startEngine();
                     car.move();
-                    int x = (int) Math.round(car.xPos);
-                    int y = (int) Math.round(car.yPos);
-                    frame.drawPanel.moveit(car, x, y);
                 }
 
                 frame.drawPanel.repaint();
@@ -90,19 +84,24 @@ public class CarController {
         }
     }
 
-    private boolean checkLoadWorkshop(Car car){
+    private boolean checkLoadWorkshop(Car car) {
+        if (car instanceof Volvo240 && Math.abs(workshops.getFirst().xPos - (int) car.xPos) <= 10 && Math.abs(workshops.getFirst().yPos - (int) car.yPos) <= 10 && !workshops.get(0).carsInShop.contains(car)) {
+            return true;
+        } else {
+            return false;
 
-
+        }
     }
 
     // Calls the gas method for each car once
     void gas(int amount) {
         double gas = ((double) amount) / 100;
         for (Vehicle car : cars
-                ) {
+        ) {
             car.gas(gas);
         }
     }
+
     void brake(int amount) {
         double brake = ((double) amount) / 100;
         for (Vehicle car : cars
--- Lab2-Old/DrawPanel.java
+++ Lab2-Old/DrawPanel.java
@@ -8,33 +8,12 @@ import javax.swing.*;
 
 // This panel represents the animated part of the view with the car images.
 
-public class DrawPanel extends JPanel{
-
+public class DrawPanel extends JPanel {
 
     CarController carC;
 
-
-
-    // Just a single image, TODO: Generalize
-    BufferedImage volvoImage;
-    BufferedImage saabImage;
-    BufferedImage scaniaImage;
-
-    // To keep track of a single car's position
-    Point volvoPoint = new Point();
-    Point saabPoint = new Point();
-
     BufferedImage volvoWorkshopImage;
-    Point volvoWorkshopPoint = new Point(300,300);
-
-    // TODO: Make this general for all cars
-    void moveit(Vehicle car,int x, int y){
-        volvoPoint.x = x;
-        volvoPoint.y = y;
-
-
-
-    }
+    Point volvoWorkshopPoint = new Point(300, 300);
 
     // Initializes the panel and reads the images
     public DrawPanel(int x, int y, CarController carC) {
@@ -43,27 +22,22 @@ public class DrawPanel extends JPanel{
         this.setBackground(Color.green);
         this.carC = carC;
         try {
-            this.volvoWorkshopImage = ImageIO.read(DrawPanel.class.getResourceAsStream("pics/VolvoBrand.jpg"));;
-
+            this.volvoWorkshopImage = ImageIO.read(DrawPanel.class.getResourceAsStream("pics/VolvoBrand.jpg"));
         } catch (IOException ex) {
             ex.printStackTrace();
         }
 
-
     }
-
 
     // This method is called each time the panel updates/refreshes/repaints itself
     // TODO: Change to suit your needs.
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-
+        g.drawImage(volvoWorkshopImage, volvoWorkshopPoint.x, volvoWorkshopPoint.y, null);
 
         for (Vehicle car : this.carC.cars) {
             // Print an error message in case file is not found with a try/catch block
-
-
             try {
                 BufferedImage img = ImageIO.read(DrawPanel.class.getResourceAsStream("pics/" + car.modelName + ".jpg"));
                 g.drawImage(img, (int) car.xPos, (int) car.yPos, null); // see javadoc for more info on the parameters
@@ -72,12 +46,7 @@ public class DrawPanel extends JPanel{
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
-
-
-        }
-
-
-            g.drawImage(volvoWorkshopImage, volvoWorkshopPoint.x, volvoWorkshopPoint.y, null);
         }
     }
+}
 
